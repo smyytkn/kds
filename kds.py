56bd860e13af6acbf21bc57f7369af8e36da4860
smyytkn <150272623+smyytkn@users.noreply.github.com>
2026-05-24 21:30:06 +0300

    Update kds.py

--- kds.py
+++ kds.py
@@ -393,6 +393,7 @@ else:
         for kod, df_sc in senaryo_listesi:
             # Senaryonun toplam maliyet serisi
             cum_sc_toplam = (df_sc["yakıt"] + df_sc["bakım"] + df_sc["taksit"]).cumsum()
+            cum_md_yakit_bakim = (df_md["yakıt"] + df_md["bakım"]).cumsum()
 
             # Kümülatif Kâr = Dizel Maliyeti - EV Maliyeti (Milyon TL cinsinden)
             cum_kar = (cum_md_yakit_bakim - cum_sc_toplam) / 1e6
